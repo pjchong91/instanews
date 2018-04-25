7f995e2cce901a9e86b7161fb8912a9be8c07d7e
Pamela Chong <pjchong91@gmail.com>
2018-04-25 16:16:42 -0700

    Loading wheel functioning

--- js/script.js
+++ js/script.js
@@ -16,42 +16,46 @@
 var itemCount = 0;
 // API MENU - Select a section to access with New York Times API
 $("#nyt-section").on("change", function() {
-  $('.results').empty();
-  var itemCount = 0;
-  var selected = $(this).val();
-  console.log(selected);
-$('body').css('height','auto');
+    $('.results').empty();
+  
+    var itemCount = 0;
+    var selected = $(this).val();
+  
+    $('body').css('height','auto');
+    $('.loading').css('display','block');
+ 
+    
+    setTimeout(function(){
+
 
   //New York Times API
   // Built by LucyBot. www.lucybot.com
-  var url = "https://api.nytimes.com/svc/topstories/v2/"+selected+".json";
-  url += '?' + $.param({
-    'api-key': "d88fc801606f4f28975b0a6761df23e7"
-  });
-  console.log(url);
-  $.ajax({
-    url: url,
-    method: 'GET',
-  })
-  .done(function(data) {
-    console.log(data.results);
-    $.each(data.results,function(key,value){
+      var url = "https://api.nytimes.com/svc/topstories/v2/"+selected+".json";
+      url += '?' + $.param({
+      'api-key': "d88fc801606f4f28975b0a6761df23e7"
+      }); //Closes $.param
 
-     if (value.multimedia.length>0){
-       console.log(value.short_url);
-       itemCount++;
+  
+      $.ajax({
+        url: url,
+        method: 'GET',
+        }) //closes .ajax
+
+      .done(function(data) {
+        $.each(data.results,function(key,value){
+          if (value.multimedia.length>0){
+            itemCount++;
+            $('.results').append('<div class="box"><a href="'+value.short_url+'"><div class="story" style="background-image: url('+value.multimedia[4].url+')"><p>'+value.abstract+'</p></div></a></div>');
+          return itemCount<12; 
+          }
+        }) //end of $.each
+      })//end of .done
       
+      .always(function(){
+          $('.loading').css('display','none');
+      })//end of .always
+     }, 3000) //end of timeout
 
-      //  $('.results').append(`<img src="${value.multimedia[4].url}">`);
-      $('.results').append('<div class="box"><a href="'+value.short_url+'"><div class="story" style="background-image: url('+value.multimedia[4].url+')"><p>'+value.abstract+'</p></div></a></div>');
-      // $(this).css('background-image','url(' + value.multimedia[4].url + ')');
-     console.log(itemCount);
-     return itemCount<12;
-      }
-        
-      })
-
-    });
 });
 
 // $("button").on("click", function() {
