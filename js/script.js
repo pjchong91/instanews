c9b4307b6158a2bf14b4d7b67829dd92366165d8
Pamela Chong <pjchong91@gmail.com>
2018-05-15 19:58:40 -0700

    Updated css and js to reflect github issues/pull request

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-$( document ).ready(() => {
+$(document).ready(() => {
 
 // API MENU - Select a section to access with New York Times API
-$('#nyt-section').on('change', function() { //??? CANT CHANGE TO ARROW
+  $('#nyt-section').on('change', function(){ //??? CANT CHANGE TO ARROW
     $('.results').empty();
     $('header').addClass('active');
  
     let selected = $(this).val();
   
-    $('body').css('height','auto');
-    $('.loading').css('display','block')
+    $('body').css('height', 'auto');
+    $('.loading').css('display', 'block')
  
-  //New York Times API from: https://developer.nytimes.com/top_stories_v2.json
-  // Built by LucyBot. www.lucybot.com
+    //New York Times API from: https://developer.nytimes.com/top_stories_v2.json
+    // Built by LucyBot. www.lucybot.com
     let url = `https://api.nytimes.com/svc/topstories/v2/${selected}.json`;
       url += '?' + $.param({
       'api-key': 'd88fc801606f4f28975b0a6761df23e7'
@@ -30,10 +30,10 @@ $('#nyt-section').on('change', function() { //??? CANT CHANGE TO ARROW
 
         $.each(goodResults, (key,value) => {
           $('.results').append(`
-          <a class="box" href="${+value.short_url}">
-            <div class="story" style="background-image: url(${value.multimedia[4].url})">
-              <p class="abstract hidden">'${value.abstract}</p>
-            </div>
+          <a class="box" href="${value.short_url}">
+            <section class="story" style="background-image: url(${value.multimedia[4].url})">
+              <p class="abstract hidden" style="width: 100%">'${value.abstract}</p>
+            </section>
           </a>`);
         }) //end of $.each
       }) //end of .done
